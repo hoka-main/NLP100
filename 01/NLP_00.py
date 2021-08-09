c67fd3600de8f7693c81e863a217461004f45445
hoka <ohisama365.not+github@gmail.com>
2021-08-09 18:51:02 +0900

    [add] index

---
+++ 01/NLP_00.py
@@ -0,0 +1,10 @@
+## 文字列”stressed”の文字を逆に（末尾から先頭に向かって）並べた文字列を得よ．
+
+def main():
+    string = 'stressed'
+    reverse_string = string[::-1]
+    print(reverse_string)
+
+
+if __name__ == '__main__':
+    main()
